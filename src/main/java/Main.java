3f2cb3f3b4565906468126d1c54790236761d087
qxeer <qxeer84@gmail.com>
2026-04-21 22:09:17 +0200

    4th stage - proof. Added a new rule and adjusted the loop to go up to 105 to present all cases

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,12 +11,14 @@ public class Main {
 
         DivisibilityRule divisibleByThree = new DivisibilityRule(3,"LOW");
         DivisibilityRule divisibleByFive = new DivisibilityRule(5,"ADVISORY");
+        DivisibilityRule divisibleBySeven = new DivisibilityRule(7, "WARN");
 
         List<RuleInterface> rules = new ArrayList<>();
         rules.add(divisibleByThree);
         rules.add(divisibleByFive);
+        rules.add(divisibleBySeven);
 
-        for(int i = 1; i <= 20; i++){
+        for(int i = 1; i <= 105; i++){
             int finalI = i;
             String alerts = rules.stream().filter(rule -> rule.match(finalI)).map(RuleInterface::getAlert).collect(Collectors.joining());
             if (alerts.isEmpty()) {
