a4210ae0cd67e9b1e45605db72f75a01f4bf3ed1
qxeer <qxeer84@gmail.com>
2026-04-21 21:38:48 +0200

    2nd stage - removing the special case by separating conditions and using StringBuilder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
         for(int i = 1; i <= 20; i++){
-
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("LOWADVISORY");
-            } else if(i % 3 == 0){
-                System.out.println("LOW");
-            } else if(i % 5 == 0){
-                System.out.println("ADVISORY");
-            } else {
-                System.out.println(i);
+            boolean matched = false;
+            if(i % 3 == 0){
+                stringBuilder.append("LOW");
+                matched = true;
             }
 
+            if(i % 5 == 0){
+                stringBuilder.append("ADVISORY");
+                matched = true;
+            }
+            if (!matched){
+                stringBuilder.append(i);
+            }
+            stringBuilder.append("\n");
+
         }
+        System.out.println(stringBuilder);
     }
 }
