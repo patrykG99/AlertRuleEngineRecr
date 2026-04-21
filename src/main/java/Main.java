fd3f4f2e018c84e91b3967a651cd3a677c56aea0
qxeer <qxeer84@gmail.com>
2026-04-21 21:27:40 +0200

    1st stage with basic implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        for(int i = 1; i <= 20; i++){
+
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("LOWADVISORY");
+            } else if(i % 3 == 0){
+                System.out.println("LOW");
+            } else if(i % 5 == 0){
+                System.out.println("ADVISORY");
+            } else {
+                System.out.println(i);
+            }
+
+        }
     }
 }
